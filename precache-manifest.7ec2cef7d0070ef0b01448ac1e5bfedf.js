ee290d1f4c780a36bfe1e42326e7138d50112b3d
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2022-08-31 19:14:00 +0200

    Updates

---
+++ precache-manifest.7ec2cef7d0070ef0b01448ac1e5bfedf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e47e385d9c958cf8cc5",
+    "url": "/todo-list-react/static/css/main.70769823.chunk.css"
+  },
+  {
+    "revision": "3e47e385d9c958cf8cc5",
+    "url": "/todo-list-react/static/js/main.3e47e385.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/todo-list-react/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/todo-list-react/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "2454914d0b5eac36b72f",
+    "url": "/todo-list-react/static/js/runtime~main.2454914d.js"
+  },
+  {
+    "revision": "960acf33d2c793906100d82f1a56ef1e",
+    "url": "/todo-list-react/index.html"
+  }
+];
