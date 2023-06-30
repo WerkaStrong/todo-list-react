09424932a3e42098e85954fb194b9690ee72c469
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2023-06-30 10:59:41 +0200

    created redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom/client';
 import { GlobalStyle} from "./GlobalStyle"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+
+const initialState = {
+  tasks: [],
+};
+
+const tasksReducer = (state = initialState, action) => {
+  if (action.type === "addTask") {
+    return {
+      ...state,
+      tasks: [
+        ...state.tasks,
+        {
+          content: action.payload,
+        },
+      ],
+    };
+  }
+
+  return state;
+
+};
+
+const store =  configureStore({ reducer: tasksReducer });
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
