bb748db6520212111ff796ec3d0968b24071e8ee
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2023-07-03 17:38:10 +0200

    persist tasks on localStorage

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -1,9 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getTaskFromLocalStorage } from "./tasksLocalStorage";
 
 const tasksSlice = createSlice({
     name: 'tasks',
     initialState: {
-        tasks: [],
+        tasks: getTaskFromLocalStorage(),
         hideDone: false,
         allDone: false,
     },
@@ -28,17 +29,31 @@ const tasksSlice = createSlice({
             const index = tasks.findIndex(({ id }) => id === taskId);
             tasks.splice(index, 1);
         },
+        fetchExampleTasks: () => { },
+        //zastępuje wszystkie zadania
+        setTasks: (state, {payload:tasks}) => {
+            state.tasks = tasks;
+        },
+
     },
 });
 
 //export funkcji addTask
-export const { addTask, toggleHideDone, toggleTaskDone, setAllDone, removeTask } = tasksSlice.actions;
+export const { 
+    addTask,
+    toggleHideDone,
+    toggleTaskDone, 
+    setAllDone, 
+    removeTask,
+    fetchExampleTasks,
+    setTasks,
+ } = tasksSlice.actions;
 //selektor (z całego state daje nam tylko tasks)
 export const selectTasksState = state => state.tasks;
 
 export const selectTasks = state => selectTasksState(state).tasks;
 export const selectHideDone = state => selectTasksState(state).hideDone;
-export const selectIsEveryTaskDone = state => selectTasks(state).every(({done}) => done);
+export const selectIsEveryTaskDone = state => selectTasks(state).every(({ done }) => done);
 export const selectIsTaskEmpty = state => selectTasks(state).length === 0;
 //export reducera
 export default tasksSlice.reducer;
