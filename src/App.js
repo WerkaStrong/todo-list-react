c4a8870102a2ebbafe52f3b7e9c87007dec6db4b
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2023-07-05 18:49:30 +0200

    added simply navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,41 @@
 import React from "react";
-import Tasks from "./features/tasks/Tasks"
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { StyledNavLink } from "./styled";
+import AuthorPage from "./features/tasks/author/AuthorPage";
 
-export default () => <Tasks />;
+export default () => (
+    <HashRouter>
+        <nav>
+            <ul>
+                <li>
+                    <StyledNavLink
+                        to="/zadania"
+                    >
+                        Zadania
+                    </StyledNavLink>
+                </li>
+                <li>
+                    <StyledNavLink
+                        to="/autor"
+                    >
+                        O autorze
+                    </StyledNavLink>
+                </li>
+            </ul>
+            <Switch>
+                <Route path="/zadania/:id">
+                    <TaskPage />
+                </Route>
+                <Route path="/">
+                    <Redirect to="/zadania" />
+                    <TasksPage />
+                </Route>
+                <Route path="/autor">
+                    <AuthorPage />
+                </Route>
+            </Switch>
+        </nav>
+    </HashRouter>
+);
