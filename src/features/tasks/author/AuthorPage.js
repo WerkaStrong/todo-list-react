9383f25e17bfabd8a4f2db067846dbf352ff4689
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2023-07-06 09:27:44 +0200

    created AuthorPage.js

---
+++ src/features/tasks/author/AuthorPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "../../../common/Container";
+import Header from "../../../common/Header";
+import Section from "../../../common/Section";
+
+export default () => (
+    <Container>
+        <Header title="O autorze"></Header>
+        <Section
+            title="Weronika Chmielewska"
+            body={<>nanana </>}
+        />
+    </Container>
+);
