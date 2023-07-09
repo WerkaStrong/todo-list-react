57a338f2a63e88189ef3e440c97d318b691ba2b0
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2023-07-09 19:40:57 +0200

    created Navigation component

---
+++ src/common/Navigation/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyledLi, StyledNav, StyledNavLink, StyledUl } from "./styled";
+
+export const Navigation = () => (
+
+    <StyledNav>
+        <StyledUl>
+
+            <StyledLi>
+                <StyledNavLink
+                    to="/zadania"
+                >
+                    Zadania
+                </StyledNavLink>
+            </StyledLi>
+            <StyledLi>
+                <StyledNavLink
+                    to="/autor"
+                >
+                    O autorze
+                </StyledNavLink>
+            </StyledLi>
+        </StyledUl>
+    </StyledNav>
+
+);
---
+++ src/common/Navigation/styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const StyledNavLink = styled(NavLink)`
+    color: white;
+    text-decoration: none;
+    
+    &.active{
+        font-weight: bold;
+    }
+`;
+
+export const StyledNav = styled.nav`
+    background-color: teal;
+    height: 65px;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const StyledUl = styled.ul`
+    margin: 0 10px;
+    display:flex;
+    flex-direction: row;
+`;
+
+export const StyledLi = styled.li`
+    margin: auto 10px;
+    list-style: none;
+`;
