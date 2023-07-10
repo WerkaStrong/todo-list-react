28222de40199f601f7f7c5f16a0cd597b518b156
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2023-07-10 15:48:05 +0200

    changed width of Buttons

--- src/features/tasks/TasksPage/Buttons/styled.js
+++ src/features/tasks/TasksPage/Buttons/styled.js
@@ -2,15 +2,16 @@ import styled, { css } from "styled-components";
 
 export const ButtonField = styled.div`
     display: flex;
-    flex-wrap: wrap;
-    align-items: center;
+    flex-direction: row;
+    justify-content: center;
 `;
 
 export const Button = styled.button`
+    box-sizing: border-box;
     background-color: white;
     border: none;
-    color:teal;
-    width: auto;
+    color: teal;
+    width: 180px;
     height: 50px ;
     transition: 1s;
     padding: 1%;
@@ -29,5 +30,6 @@ export const Button = styled.button`
         flex-basis: 100%;
         margin: 0;
         padding: 0,5%;
+        width: auto;
     }
 `;
