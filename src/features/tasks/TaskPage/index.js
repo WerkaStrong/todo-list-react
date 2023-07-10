c2632c50ad78d8d13b5c23ee1b6f7344fb6b12a2
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2023-07-10 11:31:41 +0200

    added StyledDiv to TaskPage

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -5,9 +5,10 @@ import Container from '../../../common/Container';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { getTaskById } from '../tasksSlice';
+import { StyledDiv } from './styled';
 
 function TaskPage() {
-    const {id} = useParams();
+    const { id } = useParams();
     const task = useSelector(state => getTaskById(state, id))
     return (
         <Container>
@@ -15,7 +16,11 @@ function TaskPage() {
             <Section
                 title={task ? task.content : "Nie znaleziono zadania 😢"}
                 body={
-                <><strong>Ukończono: </strong> {task.done ? "Tak" : "Nie"}</>
+                    task ? (
+                        <StyledDiv>
+                            <strong>Ukończono: </strong> {task.done ? "Tak" : "Nie"}
+                        </StyledDiv>
+                    ) : null
                 }
             />
         </Container>
---
+++ src/features/tasks/TaskPage/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledDiv= styled.div`
+    padding: 20px;
+
+    @media(max-width: 767px) {
+        padding: 1px;
+    }
+`
