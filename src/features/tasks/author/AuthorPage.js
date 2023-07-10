ddd9a9e0d2813d0780a5e0ee1a6352cf81fbe531
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2023-07-10 17:17:36 +0200

    added description to AuthorPage

--- src/features/tasks/author/AuthorPage.js
+++ src/features/tasks/author/AuthorPage.js
@@ -10,8 +10,14 @@ export default () => (
         <Section
             title="Weronika Chmielewska"
             body={
-            <StyledDiv>
-                Cześć! Nazywam się Weronika Chmielewska, jestem początkującą Fronten Developerką.
+                <StyledDiv>
+                Hej 😊 <br /><br />
+                Jestem początkującą frontend - developerką.<br /><br />
+                Kodowanie to moja pasja i nie potrafię się oderwać od ekranu, kiedy tworzę coś nowego i ciekawego. <br /><br />
+                Kiedy potrzebuję odpoczynku, zanurzam się w świecie gier komputerowych. <br /><br />
+                Lubię też aktywnie spędzać czas na świeżym powietrzu, zwłaszcza w górach, gdzie mogę zmierzyć się z własnymi słabościami. 
+                W tym roku udało mi się przejść całe Karkonosze zimą w trudnych warunkach pogodowych co jest dla mnie dużym osiągnięciem 🎉 <br /><br />
+                Już planuję kolejne podróże i wyprawy, które będą niezapomnianymi doświadczeniami! 💪
             </StyledDiv>
         }
         />
