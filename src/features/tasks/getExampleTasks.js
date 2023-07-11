14db28e63d1f8f694f17435321fb3bd892fc67bb
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2023-07-11 19:02:55 +0200

    moved adres URL to constant

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,5 +1,7 @@
+const EXAMPLE_tASKS_URL = "/todo-list-react/exampleTasks.json";
+
 export const getExampleTasks = async () => {
-    const response = await fetch("/todo-list-react/exampleTasks.json");
+    const response = await fetch(EXAMPLE_tASKS_URL);
 
     if (!response.ok) {
         new Error (response.statusText);
